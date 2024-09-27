28b8a27ddf28fcbad8aa5fd5deaa9521493385b1
Mangassouba <hamallahmanga@gmail.com>
2024-09-27 13:40:38 +0000

    Ajout la modification de recette

--- src/views/recette/modifierRecette.vue
+++ src/views/recette/modifierRecette.vue
@@ -1,58 +1,99 @@
 <template>
-    <div class="container">
-      <h2>{{ $t("recette.modify_page.titre") }}</h2>
-      <form @submit.prevent="handleSubmit">
-        <div class="mb-3">
-          <label for="title" class="form-label">{{ $t("recette.modify_page.label_titre") }}</label>
-          <input v-model="title" type="text" id="title" class="form-control" required />
-        </div>
-        <div class="mb-3">
-          <label for="ingredients" class="form-label">{{ $t("recette.modify_page.label_ingredients") }}</label>
-          <textarea v-model="ingredients" id="ingredients" class="form-control" required></textarea>
-        </div>
-        <div class="mb-3">
-          <label for="type" class="form-label">{{ $t("recette.modify_page.label_recipeType") }}</label>
-          <select v-model="type" id="type" class="form-select" required>
-            <option value="entrée">{{ $t("recette.modify_page.type_entree") }}</option>
-            <option value="plat">{{ $t("recette.modify_page.type_plat") }}</option>
-            <option value="dessert">{{ $t("recette.modify_page.type_dessert") }}</option>
-          </select>
-        </div>
-        <button type="submit" class="btn btn-primary">{{ $t("recette.modify_page.button_edit") }}</button>
-      </form>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import { useRouter, useRoute } from 'vue-router';
-import { useRecettetore } from '@/store/recetteStore';
-  
-  const store = useRecettetore();
-  const route = useRoute();
-  const router = useRouter();
-  
-  const index = route.params.index;
-  const recipe = ref({});
-  
-  const title = ref('');
-  const ingredients = ref('');
-  const type = ref('plat');
-  
-  onMounted(() => {
-    recipe.value = store.getRecipe(index);
-    title.value = recipe.value.title;
-    ingredients.value = recipe.value.ingredients;
-    type.value = recipe.value.type;
-  });
-  
-  function handleSubmit() {
-    store.updateRecipe(index, {
-      title: title.value,
-      ingredients: ingredients.value,
-      type: type.value
-    });
-    router.push({ name: 'recette' });
+  <div class="container">
+    <h2>{{ $t("recette.modify_page.titre") }}</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="mb-3">
+        <label for="title" class="form-label">{{
+          $t("recette.modify_page.label_titre")
+        }}</label>
+        <input
+          v-model="titre"
+          type="text"
+          id="titre"
+          class="form-control"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <label for="type" class="form-label">{{
+          $t("recette.modify_page.label_recipeType")
+        }}</label>
+        <select v-model="type" id="type" class="form-select" required>
+          <option value="entrée">
+            {{ $t("recette.modify_page.type_entree") }}
+          </option>
+          <option value="plat">
+            {{ $t("recette.modify_page.type_plat") }}
+          </option>
+          <option value="dessert">
+            {{ $t("recette.modify_page.type_dessert") }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="ingredients" class="form-label">{{
+          $t("recette.modify_page.label_ingredients")
+        }}</label>
+        <textarea
+          v-model="ingredient"
+          id="ingredient"
+          class="form-control"
+          required
+        ></textarea>
+      </div>
+      <div class="mb-3">
+        <label for="title" class="form-label">{{
+          $t("recette.modify_page.label_titre")
+        }}</label>
+        <input
+          v-model="category_id"
+          type="number"
+          id="category_id"
+          class="form-control"
+          required
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">
+        {{ $t("recette.modify_page.button_edit") }}
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { useRecettetore } from "@/store/recetteStore";
+
+const store = useRecettetore();
+const route = useRoute();
+const router = useRouter();
+
+const id = route.params.id;
+const recipe = ref({});
+
+const titre = ref("");
+const ingredient = ref("");
+const type = ref("");
+const category_id = ref("");
+
+onMounted(() => {
+  const recipe = store.getRecipe(parseInt(route.params.id));
+  if (recipe) {
+    titre.value = recipe.titre;
+    type.value = recipe.type;
+    ingredient.value = recipe.ingredient;
+    category_id.value = recipe.category_id;
   }
-  </script>
-  
+});
+
+function handleSubmit() {
+  store.updateRecipe(id, {
+    titre: titre.value,
+    ingredient: ingredient.value,
+    type: type.value,
+    category_id: category_id.value,
+  });
+  router.push({ name: "recette" });
+}
+</script>
